21dc22fbb9767515b0f50e7afa11d6045fbe1a4e
Samuel VIDEAU <samuel.videau@efrei.net>
2021-12-15 18:36:09 +0100

    test: display the tasks

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
-import {assert} from 'chai';
+import {assert, expect} from 'chai';
 import {getOperator, getContent} from "../src/utils/parsing";
 import {TaskManager} from "../src/TaskManager";
+import {Task} from "../src/models/task.model";
 
 describe('Parsing', () => {
 
@@ -52,5 +53,49 @@ describe('Update the task list', () => {
     taskManager.parse('+ Unit Test');
     taskManager.parse('o 0');
     assert(taskManager.getTaskList()[0].status === 'to do');
+  });
+});
+
+describe('Display the tasks', () => {
+  let taskManager: TaskManager;
+  beforeEach(() => {
+    taskManager = new TaskManager();
+    taskManager.parse('+ Unit Test');
+    taskManager.parse('+ Unit Test2');
+    taskManager.parse('+ Unit Test3');
+  });
+
+  it('should display the expected amount of lines', () => {
+    const display = taskManager.displayTaskList().split('\n');
+    expect(display.length).to.equal(taskManager.getTaskList().length);
+  });
+
+  it('should display the expected ids', () => {
+    const display: string[] = taskManager.displayTaskList().split('\n');
+    const tasks: Task[] = taskManager.getTaskList();
+    for(let i = 0; i < display.length; i++ ) {
+      expect(display[i]).to.contain(tasks[i].id + ' [');
+    }
+  });
+
+  it('should display the expected descriptions', () => {
+    const display: string[] = taskManager.displayTaskList().split('\n');
+    const tasks: Task[] = taskManager.getTaskList();
+    for(let i = 0; i < display.length; i++ ) {
+      expect(display[i]).to.contain(tasks[i].description);
+    }
+  });
+
+  it('should display the expected to do status', () => {
+    const display: string[] = taskManager.displayTaskList().split('\n');
+    for(let i = 0; i < display.length; i++ ) {
+      expect(display[i]).to.contain('[ ]');
+    }
+  });
+
+  it('should display the expected done status', () => {
+    taskManager.parse('x 1');
+    const display: string[] = taskManager.displayTaskList().split('\n');
+    expect(display[0]).to.contain('[x]');
   });
 });
