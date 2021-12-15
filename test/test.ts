e99474cedab75872390d3f1e49f0a8c769f36cff
Samuel VIDEAU <samuel.videau@efrei.net>
2021-12-15 11:39:21 +0100

    tests: parsing + operator

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,20 @@
 import {assert} from 'chai';
-import {main} from "../src";
 
-describe('Task Manager', () => {
-  it('should not throw', () => {
-    try {
-      main();
-    } catch (e) {
-      assert(false);
-    }
-    console.assert(true);
+describe('Parsing operator +', () => {
+
+  beforeEach(() => {
+    parse('+ Unit Test');
+  });
+
+  it('should have increased task list', () => {
+    assert(getTaskList.length > 0);
+  });
+
+  it('should parse correctly the description', () => {
+    assert(getTaskList[0].description === 'Unit Test');
+  });
+
+  it('should have done status to false', () => {
+    assert(getTaskList[0].description === 'Unit Test');
   });
 });
