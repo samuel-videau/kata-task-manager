422ef00412462ab74b7796b08962f3c965018388
Samuel VIDEAU <samuel.videau@efrei.net>
2021-12-15 11:44:06 +0100

    tests: parsing operators

--- test/test.ts
+++ test/test.ts
@@ -1,20 +1,20 @@
 import {assert} from 'chai';
 
-describe('Parsing operator +', () => {
+describe('Parsing', () => {
 
-  beforeEach(() => {
-    parse('+ Unit Test');
+  it('should parse + operator', () => {
+    assert(getOperator('+ Unit Test'));
   });
 
-  it('should have increased task list', () => {
-    assert(getTaskList.length > 0);
+  it('should parse - operator', () => {
+    assert(getOperator('- Unit Test'));
   });
 
-  it('should parse correctly the description', () => {
-    assert(getTaskList[0].description === 'Unit Test');
+  it('should parse x operator', () => {
+    assert(getOperator('x Unit Test'));
   });
 
-  it('should have done status to false', () => {
-    assert(getTaskList[0].description === 'Unit Test');
+  it('should parse o operator', () => {
+    assert(getOperator('o Unit Test'));
   });
 });
