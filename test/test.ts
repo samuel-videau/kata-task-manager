f589055438f3f08c8b8a305f74ced1b48ed7b2e5
Samuel VIDEAU <samuel.videau@efrei.net>
2021-12-15 11:16:03 +0100

    refactor: testing op

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,13 @@
+import {assert} from 'chai';
+import {main} from "../src";
 
+describe('Task Manager', () => {
+  it('should not throw', () => {
+    try {
+      main();
+    } catch (e) {
+      assert(false);
+    }
+    console.assert(true);
+  });
+});
